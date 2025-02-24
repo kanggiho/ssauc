bd6ccdbb198cd379df9c8a068a96c2c82a9b569f
kanggiho <kgh5554@naver.com>
2025-02-24 17:23:43 +0900

    feat : html create complete

--- src/main/java/com/example/ssauc/user/order/controller/OrderController.java
+++ src/main/java/com/example/ssauc/user/order/controller/OrderController.java
@@ -1,7 +1,15 @@
 package com.example.ssauc.user.order.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("order")
 public class OrderController {
+    @GetMapping("order")
+    public String order(Model model) {
+        return "order/order";
+    }
 }
--- src/main/java/com/example/ssauc/user/pay/controller/PayController.java
+++ src/main/java/com/example/ssauc/user/pay/controller/PayController.java
@@ -1,7 +1,14 @@
 package com.example.ssauc.user.pay.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("pay")
 public class PayController {
+    @GetMapping("pay")
+    public String pay() {
+        return "pay/pay";
+    }
 }
