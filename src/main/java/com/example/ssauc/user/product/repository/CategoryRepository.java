030e7d49b82be4f8f62631031ffa7fb46eeb8ee9
gunny <gunny97h@gmail.com>
2025-02-25 10:47:44 +0900

    feat: Add product insert files (#57)
    - S3 이미지 제외하고 상품 등록 기능 구현

--- src/main/java/com/example/ssauc/user/product/repository/CategoryRepository.java
+++ src/main/java/com/example/ssauc/user/product/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.example.ssauc.user.product.repository;
 import com.example.ssauc.user.product.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
 }
