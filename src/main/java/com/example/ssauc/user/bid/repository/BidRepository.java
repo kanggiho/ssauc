84fd2d138f777c673e655a8699510e19aa2b109f
gunny <gunny97h@gmail.com>
2025-03-07 20:47:39 +0900

    feat: Add bid history java file (#140)

--- src/main/java/com/example/ssauc/user/bid/repository/BidRepository.java
+++ src/main/java/com/example/ssauc/user/bid/repository/BidRepository.java
@@ -1,9 +1,15 @@
 package com.example.ssauc.user.bid.repository;
 
 import com.example.ssauc.user.bid.entity.Bid;
+import com.example.ssauc.user.login.entity.Users;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BidRepository extends JpaRepository<Bid, Long> {
+import java.time.LocalDateTime;
 
+public interface BidRepository extends JpaRepository<Bid, Long> {
+    // 현재 시간보다 경매 마감 시간이 지나지 않은 것만 조회
+    Page<Bid> findByUserAndProductEndAtAfter(Users user, LocalDateTime now, Pageable pageable);
 
 }
