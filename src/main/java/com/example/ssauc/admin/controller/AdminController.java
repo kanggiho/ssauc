a7ac6a2422118893471a0a7f27aba05f95cc207b
kanggiho <kgh5554@naver.com>
2025-03-10 18:05:21 +0900

    feat: 관리자 로그인 및 메인화면 구현

--- src/main/java/com/example/ssauc/admin/controller/AdminController.java
+++ src/main/java/com/example/ssauc/admin/controller/AdminController.java
@@ -1,8 +1,20 @@
 package com.example.ssauc.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/admin")
 @Controller
 public class AdminController {
 
+    @GetMapping
+    public String index() {
+        return "/admin/admin";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "/admin/adminhome";
+    }
 }
